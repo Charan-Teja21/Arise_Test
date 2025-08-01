38349a8ce19d243988a55293f3fd75b1884908f9
Maka Charan Teja <charant921@gmail.com>
2025-08-01 12:25:03 +0530

    feature1 added

--- frontend/temp_collab_frontend.js
+++ frontend/temp_collab_frontend.js
@@ -1,2 +1,3 @@
 // Temporary file for frontend collaboration testing
 console.log('Frontend collaboration test file created.');
+console.log('This file is for testing frontend collaboration features.');
